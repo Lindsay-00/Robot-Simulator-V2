5b4c06be0b8ccb2f290ac543176fa3ebc4296c88
Lindsay <lindsay.chen@yale.edu>
2021-08-13 06:08:57 +0800

    Done

--- index.js
+++ index.js
@@ -1,6 +1,45 @@
+
 document.addEventListener("DOMContentLoaded", function() {
   // initializes the board
   init();
 
   // ADD CODE HERE!
-});
+  const moves = document.querySelector("#moves-container")
+  const moveBtn = document.querySelector("#move-button")
+
+  document.addEventListener("keydown", (evnt) => {
+    const move = document.createElement('li')
+    if (evnt.key === "ArrowUp") {
+      move.textContent = "up"
+    }
+    else if (evnt.key === "ArrowDown") {
+      move.textContent = "down"
+    }
+    else if (evnt.key === "ArrowRight") {
+      move.textContent = "right"
+    }
+    else if (evnt.key === "ArrowLeft") {
+      move.textContent = "left"
+    }
+    moves.append(move)
+  })
+
+  moveBtn.addEventListener('click', (evnt) => {
+
+    
+    let timer = setInterval(() => {
+      const currMove = moves.querySelector('li')
+      if (currMove != null) {
+        move(currMove.textContent)
+        currMove.remove()
+      }
+      else {
+        clearInterval(timer)
+      }
+    }, 500)
+
+
+  })
+
+
+})
